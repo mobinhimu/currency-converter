1bd71bc3e8a40205e20cd612ef1b3c75ba5b26ca
Mobin <89353916+mobinhimu@users.noreply.github.com>
2023-07-31 19:12:31 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./currency-converter/index.css";
-import App from "./currency-converter/App";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
